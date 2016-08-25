aee6ccef81728ac17e9af758231da6b9726c0039
Tute Costa <tutecosta@gmail.com>
2016-08-25 11:55:13 -0400

    Add regression test and fix tests
    Translation files should be subset of `en`, as much as `en` should be
    subset of translation files.
    
    Fixes missing translations.
    
    [fixes #19]
    [fixes #20]

--- spec/doorkeeper-i18n_spec.rb
+++ spec/doorkeeper-i18n_spec.rb
@@ -4,5 +4,6 @@ Dir.glob('rails/locales/*.yml').each do |locale_file|
   describe "a doorkeeper-i18n #{locale_file} locale file" do
     it_behaves_like 'a valid locale file', locale_file
     it { expect(locale_file).to be_a_subset_of 'rails/locales/en.yml' }
+    it { expect('rails/locales/en.yml').to be_a_subset_of locale_file }
   end
 end
